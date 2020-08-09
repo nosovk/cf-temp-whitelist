76ae1536792212b5b9008dcb8d93773e39bbd11e
Nosov Konstantin <nosov@nodeart.io>
2020-08-09 16:31:38 +0300

    fix typo pretty if\else

--- api/add.js
+++ api/add.js
@@ -6,27 +6,29 @@ const account = process.env['account'] || 'cloudflare account id'
 const tempMark = process.env['tempmark'] || 'temp'
 
 module.exports = (req, res) => {
-    cf.accountAccessFirewall.add(account,{mode:"whitelist",configuration:{
+    cf.accountAccessFirewall.add(account, {
+        mode: "whitelist", configuration: {
             "target": "ip",
             "value": req.headers['x-real-ip']
-        },notes:tempMark}).then(cfResponce=>{
-        return res.status(200).send({listed: cfResponce.success, ip: req.headers['x-real-ip']});
+        }, notes: tempMark
+    }).then(response => {
+        return res.status(200).send({listed: response.success, ip: req.headers['x-real-ip']});
     })
-        .catch(err=>{
+        .catch(err => {
             if (err.statusCode !== 400) {
                 return res.status(500).send({listed: false, err: JSON.stringify(err)});
-            } else {
-                cf.accountAccessFirewall.browse(account).then(ips=> {
-                        let record = ips.result.find(rec => {
-                            return rec.configuration.target === "ip" && rec.configuration.value === req.headers['x-real-ip']
-                        })
-                        if (record) {
-                            return res.status(200).send({listed: true, ip: req.headers['x-real-ip']});
-                        } else {
-                            return res.status(500).send({listed: false, err: JSON.stringify(err)});
-                        }
-                    }
-                )
             }
+            cf.accountAccessFirewall.browse(account).then(ips => {
+                    let record = ips.result.find(rec => {
+                        return rec.configuration.target === "ip" && rec.configuration.value === req.headers['x-real-ip']
+                    })
+                    if (record) {
+                        return res.status(200).send({listed: true, ip: req.headers['x-real-ip']});
+                    }
+                    return res.status(500).send({listed: false, err: JSON.stringify(err)});
+
+                }
+            )
+
         })
 };
